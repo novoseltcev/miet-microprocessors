2311243dbc6ebe1250ec92479876eeb9934202fb
st-a-novoseltcev <st.a.novoseltcev@gmail.com>
2021-12-12 05:11:27 +0300

    reformat RegFile.v

--- src/RegFile.v
+++ src/RegFile.v
@@ -1,18 +1,18 @@
 module RegFile (
   input clk,
-  input [4:0] Address_1, Address_2, Address_3,
-  input  	    WriteEnable_3,
-  input	     [31:0] WriteData_3,
-  output reg [31:0] ReadData_1, ReadData_2
+  input [4:0] address_1, address_2, address_3,
+  input  	    write_enable_3,
+  input	     [31:0] write_data_3,
+  output reg [31:0] read_data_1, read_data_2
 );
 
   reg [31:0] RAM [0:31];
   
-  assign ReadData_1 = (Address_1 == 0) ? 32'b0 : RAM[Address_1];
-  assign ReadData_2 = (Address_2 == 0) ? 32'b0 : RAM[Address_2];
+  assign read_data_1 = (address_1 == 0) ? 32'b0 : RAM[address_1];
+  assign read_data_2 = (address_2 == 0) ? 32'b0 : RAM[address_2];
   
   always @(posedge clk)
-    if (WriteEnable_3) 
-      RAM[Address_3] <= WriteData_3;
+    if (write_enable_3) 
+      RAM[address_3] <= write_data_3;
 
 endmodule
